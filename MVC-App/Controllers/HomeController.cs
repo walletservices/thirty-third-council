3dde81af1c5c3e57a143e23cf63273b5f0580ce6
Stewart Pavitt <stewart@wallet.services>
2019-09-20 08:29:02 +0100

    Removed Graph connecotr, unwanted async message and unwated libs

--- MVC-App/Controllers/HomeController.cs
+++ MVC-App/Controllers/HomeController.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Security.Claims;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using Microsoft.Identity.Client;
-using MVC_App.Models;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MVC_App
 {
@@ -47,7 +34,7 @@ namespace MVC_App
             return View();
         }
         [Authorize]
-        public async Task<IActionResult> Api()
+        public IActionResult Api()
         {
             string idToken = HttpContext.User.FindFirst("id_token").Value;
 

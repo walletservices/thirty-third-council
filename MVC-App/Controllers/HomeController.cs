64b71892b86e3d54d3ee2c089a72c5dc6a521a94
Liam Bell <liam@wallet.services>
2019-09-26 11:10:19 +0100

    Http Post for Claims and Attestations

--- MVC-App/Controllers/HomeController.cs
+++ MVC-App/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Data;
 using System.ComponentModel;
 using static MVC_App.ProcessModel;
+using System.Net.Http;
 
 namespace MVC_App
 {
@@ -19,7 +20,7 @@ namespace MVC_App
         ISiccarConnector _connector;
         ISiccarConfig _config;
 
-        public HomeController(ISiccarConnector connector, ISiccarConfig config)
+        public HomeController(ISiccarConnector connector, ISiccarConfig config )
         {
             _connector = connector;
             _config = config;
@@ -34,7 +35,7 @@ namespace MVC_App
         {
             var idToken = HttpContext.User.FindFirst("id_token").Value;
             var resp = "[{\"schemaId\":\"0410b038-6d50-4568-ae29-6b21eace1581\",\"schemaInstanceId\":\"b76583b9-2737-416c-abc6-47fed57c8dd2\",\"schemaTitle\":\"Simple Two Step Private Wallet \",\"stepStatuses\":[{\"stepIndex\":0,\"stepTitle\":\"Single User Wallet \",\"completionTime\":1563192239},{\"stepIndex\":1,\"stepTitle\":\"Please authorise \",\"completionTime\":1563192281}]},{\"schemaId\":\"0c189e4f-e1d2-440c-867b-115d211b458d\",\"schemaInstanceId\":\"76bb0153-aeb8-4a2c-a87b-b5f58ea056ca\",\"schemaTitle\":\"Process A for Phase  3\",\"stepStatuses\":[{\"stepIndex\":0,\"stepTitle\":\"Application Form for Blue Badge\",\"completionTime\":1569230795},{\"stepIndex\":1,\"stepTitle\":\"Validate\",\"completionTime\":1569251755},{\"stepIndex\":2,\"stepTitle\":\"Authorise\",\"completionTime\":null}]},{\"schemaId\":\"0c189e4f-e1d2-440c-867b-115d211b458d\",\"schemaInstanceId\":\"dfe02c3a-8d64-4879-a9b5-c02f89cecd8b\",\"schemaTitle\":\"Process A for Phase  3\",\"stepStatuses\":[{\"stepIndex\":0,\"stepTitle\":\"Application Form for Blue Badge\",\"completionTime\":1569240435},{\"stepIndex\":1,\"stepTitle\":\"Validate\",\"completionTime\":1569251708},{\"stepIndex\":2,\"stepTitle\":\"Authorise\",\"completionTime\":null}]},{\"schemaId\":\"0c189e4f-e1d2-440c-867b-115d211b458d\",\"schemaInstanceId\":\"e98769ec-7cf5-4055-b1b0-4c22697808df\",\"schemaTitle\":\"Process A for Phase  3\",\"stepStatuses\":[{\"stepIndex\":0,\"stepTitle\":\"Application Form for Blue Badge\",\"completionTime\":1569243932},{\"stepIndex\":1,\"stepTitle\":\"Validate\",\"completionTime\":1569251668},{\"stepIndex\":2,\"stepTitle\":\"Authorise\",\"completionTime\":null}]},{\"schemaId\":\"0c189e4f-e1d2-440c-867b-115d211b458d\",\"schemaInstanceId\":\"a27a2f1f-66b2-4af2-b583-19684d5b34d4\",\"schemaTitle\":\"Process A for Phase  3\",\"stepStatuses\":[{\"stepIndex\":0,\"stepTitle\":\"Application Form for Blue Badge\",\"completionTime\":1569245952},{\"stepIndex\":1,\"stepTitle\":\"Validate\",\"completionTime\":1569246121},{\"stepIndex\":2,\"stepTitle\":\"Authorise\",\"completionTime\":null}]}]";
-            var response = await _connector.GetProgressReport(idToken);
+            //var response = await _connector.GetProgressReport(idToken);
             
             JArray jsonObject = JArray.Parse(resp);
             var processSchemaList = jsonObject.ToObject<List<ProcessSchema>>();
@@ -65,9 +66,12 @@ namespace MVC_App
         [Authorize]
         public async Task<IActionResult> StartProcessA()
         {
+
             var idToken = HttpContext.User.FindFirst("id_token").Value;
             var content = await _connector.GetStepNextOrStartProcess(_config.ProcessA, _config.ProcessAVersion, idToken);
             dynamic model = JsonConvert.DeserializeObject(content);
+            var attestationToken = await extendTokenAttestation();
+            HttpContext.Session.SetString("attestationToken", attestationToken);
             ViewData["Payload"] = content;
             return View("Api", model);
 
@@ -79,6 +83,8 @@ namespace MVC_App
             var idToken = HttpContext.User.FindFirst("id_token").Value;
             var content = await _connector.GetStepNextOrStartProcess(_config.ProcessB, _config.ProcessBVersion, idToken);
             dynamic model = JsonConvert.DeserializeObject(content);
+
+
             ViewData["Payload"] = content;
             return View("Api", model);
 
@@ -92,6 +98,41 @@ namespace MVC_App
             dynamic model = JsonConvert.DeserializeObject(content);
             ViewData["Payload"] = content;
             return View("Api", model);
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<string> extendTokenAttestation()
+        {
+            HttpClient client = new HttpClient();
+
+            var config = new Dictionary<string, string>();
+            config.Add("client_id", "thirty-third-council");
+            config.Add("grant_type", "wallettoattestations");
+            config.Add("token", HttpContext.User.FindFirst("id_token").Value);
+            config.Add("scopes", "field_attestation, has_blue_badge, fork_handles, four_candles");
+            var httpContent = new FormUrlEncodedContent(config);
+
+
+            var response = await client.PostAsync("https://localhost:8691/connect/token", httpContent);
+
+            return response.Content.ReadAsStringAsync().Result;
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<string> extendTokenClaims()
+        {
+            HttpClient client = new HttpClient();
+
+            var config = new Dictionary<string, string>();
+            config.Add("client_id", "thirty-third-council");
+            config.Add("grant_type", "wallettoclaims");
+            config.Add("token", HttpContext.User.FindFirst("id_token").Value);
+            config.Add("scopes", "field_name, pet_name, first_name, last_name, date_of_birth");
+            var httpContent = new FormUrlEncodedContent(config);
+
+            var response = await client.PostAsync("https://localhost:8691/connect/token", httpContent);
+
+            return response.Content.ReadAsStringAsync().Result;
         }
 
         [Authorize]
